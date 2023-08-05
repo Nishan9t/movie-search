7d643d6dfbd636eca06f2e6e6b3378a89268b176
Nishan9t <pandeynisha9t@gmail.com>
2023-08-05 16:03:59 +0530

    api data is fetching

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [movie, setMovie] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const url = `http://www.omdbapi.com/?apikey=aee4729a&s=${movie}`;
+  const getMovieData = async () => {
+    const apiResponse = await fetch(url);
+    const data = await apiResponse.json();
+    if (data.Search) {
+      setData(data.Search);
+      console.log(data);
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getMovieData();
+  }, []);
+  
   return (
-    <div className="bg-red-500">
-      Hello
-    </div>
+    <div className="App">
+    <input
+      type="text"
+      value={movie}
+      onChange={(e) => setMovie(e.target.value)}
+    />
+    <button onClick={() => getMovieData()}>Show movie</button>
+
+    <br />
+ 
+  </div>
   );
 }
 
